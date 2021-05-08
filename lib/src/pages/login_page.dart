801fa0d3d8d5a867f907849fc15b41b37df7a660
ekographs <nwaforlorekoate@hotmail.com>
2021-05-08 20:33:40 +0400

    login page in sight

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -9,14 +9,45 @@ class _LoginPageState extends  State<LoginPage>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 15.0),
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget> [
-            Text("Sign In", style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold,),),
-          ],
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+        gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        colors: [
+        Colors.orange[900],
+        Colors.orange[800],
+        Colors.orange[400]
+        ]
+      ),
         ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 80,),
+            Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text("Login", style: TextStyle(color: Colors.white, fontSize: 45),),
+              SizedBox(height: 10,),
+              Text("Hi There, Welcome Back", style: TextStyle(color: Colors.white, fontSize: 15),),
+
+          ],
+            )
+            ),
+            SizedBox(height: 30,),
+            Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(70), topRight: Radius.circular(70)),
+                  ),
+                ),
+            )
+          ],
+      ),
       ),
     );
   }
